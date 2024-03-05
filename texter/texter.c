4bb14409def810ba96d6decf660e16acdde810d8
Walerep <isakov2594@gmail.com>
2024-03-05 14:52:41 +0700

    added find function

--- texter/texter.c
+++ texter/texter.c
@@ -216,6 +216,19 @@ int editorRowCxToRx(erow *row, int cx) {
 	return rx;
 }
 
+int editorRowRxToCx(erow *row,int rx) {
+	int cur_rx = 0;
+	int cx;
+	for (cx = 0; cx < row->size; cx++) {
+		if (row->chars[cx] == '\t') 
+			cur_rx += (KILO_TABS_STOP - 1) - (cur_rx & KILO_TABS_STOP);
+		cur_rx++;
+
+		if (cur_rx > rx) return cx;
+	}
+	return cx;
+}
+
 void editorUpdateRow(erow *row) {
 	int tabs = 0;
 	int j;
@@ -411,6 +424,25 @@ void editorSave() {
 	editorSetStatusMessage("Can't save! I/o error: %s", strerror(errno));
 }
 
+/*	find	*/
+
+void editorFind() {
+	char *query = editorPrompt("Seach: %s (ESC to cancel)");
+	if (query == NULL) return;
+
+	int i;
+	for (i = 0; i < E.numrows; i++) {
+		erow *row = &E.row[i];
+		char *match = strstr(row->render, query);
+		if (match) {
+			E.cy = i;
+			E.cx = editorRowRxToCx(row, match - row->render);
+			E.rowoff = E.numrows;
+			break;
+		}
+	}
+	free(query);
+}
 
 /*	input	*/
 
@@ -510,6 +542,10 @@ void editorProcessKeypress() {
 		break;
 	case END_KEY:
 		if (E.cy < E.numrows) E.cx = E.row[E.cy].size;
+		break;
+
+	case CTRL_KEY('f'):
+		editorFind();
 		break;
 
 	case BACKSPACE:
@@ -695,7 +731,7 @@ int main(int argc, char *argv[]) {
 		editorOpen(argv[1]);
 	}
 
-	editorSetStatusMessage("HELP: Ctrl-Q=quit | Ctrl-S=save | Ctrl-F=find");
+	editorSetStatusMessage("HELP: Ctrl-Q = quit | Ctrl-S = save | Ctrl-F = find");
 
   while(1) {
 		editorRefreshScreen();
