c35fe2c988c917a4849b2cbbc46316e97b954a85
Walerep <isakov2594@gmail.com>
2024-03-05 15:19:58 +0700

    Added incremental search

--- texter/texter.c
+++ texter/texter.c
@@ -1,3 +1,10 @@
+/*
+	Credits to: https://github.com/snaptoken
+	Source: https://viewsourcecode.org/snaptoken/kilo/index.html
+
+	A simple text editor written as part of a learning.
+*/
+
 #define _DEFAULT_SOURCE
 #define _BSD_SOURCE
 #define _GNU_SOURCE
@@ -82,7 +89,7 @@ enum editorKey {
 
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
 /*	utility functions	*/
 void die(const char *s) {
@@ -397,7 +404,7 @@ void editorOpen(char *filename) {
 
 void editorSave() {
 	if(E.filename == NULL) {
-		E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+		E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
 		if (E.filename == NULL) {
 			editorSetStatusMessage("Save aborted");
 			return;
@@ -426,9 +433,12 @@ void editorSave() {
 
 /*	find	*/
 
-void editorFind() {
-	char *query = editorPrompt("Seach: %s (ESC to cancel)");
-	if (query == NULL) return;
+void editorFindCallback(char *query , int key) {
+  if (key == '\r' || key == '\x1b') {
+    return;
+  }
+
+  if (query == NULL) return;
 
 	int i;
 	for (i = 0; i < E.numrows; i++) {
@@ -441,12 +451,19 @@ void editorFind() {
 			break;
 		}
 	}
-	free(query);
+}
+
+void editorFind() {
+	char *query = editorPrompt("Seach: %s (ESC to cancel)", editorFindCallback);
+	
+	if (query) {
+    free(query);
+  }
 }
 
 /*	input	*/
 
-char *editorPrompt(char *prompt) {
+char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
 	size_t bufsize = 128;
 	char *buf = malloc(bufsize);
 
@@ -462,11 +479,13 @@ char *editorPrompt(char *prompt) {
 			if (buflen != 0) buf[--buflen] = '\0';
 		} else if(c == '\x1b') {
 			editorSetStatusMessage("");
+			if (callback) callback(buf, c);
 			free(buf);
 			return NULL;
 		} else if (c == '\r') {
 			if (buflen != 0) {
 				editorSetStatusMessage("");
+				if (callback) callback(buf, c);
 				return buf;
 			}
 		} else if (!iscntrl(c) && c < 128) {
@@ -477,6 +496,7 @@ char *editorPrompt(char *prompt) {
 			buf[buflen++] = c;
 			buf[buflen] = '\0';
 		}
+		if (callback) callback(buf, c);
 	}
 }
 
